a1360850a7ef9bbf4b7daf7fe388ef428ff020e3
unknown <muhammadmubashirak329@gmail.com>
2024-11-17 04:05:16 +0500

    master

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
